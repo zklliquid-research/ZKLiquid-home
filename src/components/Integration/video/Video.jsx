263ce573adbf5c7b39814970f5ba3317e0d52c31
zklliquid-research <88162641+zklliquid-research@users.noreply.github.com>
2025-03-05 11:31:05 +0300

    Update Video.jsx

--- src/components/Integration/video/Video.jsx
+++ src/components/Integration/video/Video.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function Video() {
   useEffect(() => {
     // Replace the current URL with the YouTube video link when the component mounts
-    window.location.replace("https://www.youtube.com/watch?v=NR-JywFofY0");
+    window.location.replace("https://www.youtube.com/watch?v=Vr5yI9xJWOM");
   }, []);
 
   return <div>Video</div>;
